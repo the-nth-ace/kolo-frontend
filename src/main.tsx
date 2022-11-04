95519622f1a4050ed2a8225fe1d587b430aeb1f2
Femi Olatubosun <thefemiolatubosun@gmail.com>
2022-11-04 15:10:15 +0100

    Added react-router

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,26 @@ import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme";
 import Fonts from "./components/Fonts/index";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import { LandingPage } from "./pages";
+import Dashboard from "./pages/Dashboard";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+]);
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
+      <RouterProvider router={router} />
       <Fonts />
-      <App />
     </ChakraProvider>
   </React.StrictMode>
 );
