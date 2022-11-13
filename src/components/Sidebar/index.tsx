65550eb674a098ec77e5349d07648160bb1e4507
Muhammad-Tahir <mtz5prif@gmail.com>
2022-11-13 16:40:44 +0100

    sidebar refactoring

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -6,11 +6,48 @@ import {
   List,
   ListItem,
   Spacer,
+  Icon, 
+  Tooltip
 } from "@chakra-ui/react";
 import React from "react";
 import { AiOutlinePoweroff } from "react-icons/ai";
 import { navData } from "./nav-data";
-import NavItem from "./nav-item";
+import { SidebarItem as Item } from "../../types/nav-item";
+import { Link } from "react-router-dom";
+
+
+type Props = {
+  item: Item;
+  isActive: boolean;
+};
+const NavItem: React.FC<Props> = ({ item, isActive }: Props) => {
+  return (
+    <Link to="">
+      <HStack
+        align="center"
+        justify="flex-start"
+        height={{ base: 10, "2xl": 14 }}
+        transition="ease-out"
+        transitionProperty="backgroud"
+        transitionDuration="normal"
+      >
+        <Tooltip label={item.label} placement="right">
+          <span>
+            <Icon
+              w={5}
+              h={5}
+              color={isActive ? "header" : "body"}
+              _hover={{
+                color: "white",
+              }}
+              as={item.icon}
+            />
+          </span>
+        </Tooltip>
+      </HStack>
+    </Link>
+  );
+};
 
 const Sidebar: React.FC = () => {
   return (
